305ccc58d9fdbbf367266b778a70b4e86e2b7dc0
Bilal Karademir <34405358+cerkezbilal@users.noreply.github.com>
2023-07-21 15:54:46 +0300

    EnemyManager and Object Pool System created
    Enemy Manager oluşturduk ve Object Pool sistemine uygun bir şekilde düzenledik

--- DontStopRun/Assets/GameFolders/Scripts/Concretes/Controllers/SpawnerController.cs
+++ DontStopRun/Assets/GameFolders/Scripts/Concretes/Controllers/SpawnerController.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using DontStopRun.Managers;
 using UnityEngine;
 
 namespace DontStopRun.Controllers
 {
     public class SpawnerController : MonoBehaviour
     {
-        //Bunu ileride değiştircez şuan deneme amaçlı
-        [SerializeField] EnemyController _enemyPrefab;
+      
+        
         [Range(0.1f,5f)]
         [SerializeField] float _min = 0.1f;
         [Range(6f, 15f)]
@@ -41,8 +42,10 @@ namespace DontStopRun.Controllers
         void Spawn()
         {
             //düşman Oluşturma işlemi.
-           EnemyController newEnemy = Instantiate(_enemyPrefab, transform.position, transform.rotation);
+            EnemyController newEnemy = EnemyManager.Instace.GetPool();
             newEnemy.transform.parent = this.transform;
+            newEnemy.transform.position = this.transform.position;
+            newEnemy.gameObject.SetActive(true);
             _currentSpanwTime = 0f;
             GetRondomMaxTime();//Her düşman oluştuktan sonra.
         }
---
+++ DontStopRun/Assets/GameFolders/Scripts/Concretes/Managers/EnemyManager.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DontStopRun.Abstract.Utilities;
+using DontStopRun.Controllers;
+using UnityEngine;
+
+namespace DontStopRun.Managers
+{
+    public class EnemyManager : SingletonMonoBehaviorObject<EnemyManager>
+    {
+        [SerializeField] EnemyController _enemyPrefab;
+
+        //Bir liste türüdür ama farkı sırası yoktur. Queue : Kuyruk demektir
+        Queue<EnemyController> _enemies = new Queue<EnemyController>();
+
+        private void Awake()
+        {
+            SingletonThisObject(this);
+        }
+
+        private void Start()
+        {
+            InitializePool();
+        }
+
+        //Pool Sistemi oluştur demek Bu bir kere çalışcaz oyun ilk açıldığında
+        void InitializePool()
+        {
+            //sayıyı arttırabilirsiniz kurşun olsa daha fazla yapardık ama bizim
+           
+            for (int i = 0;  i< 10; i++)
+            {
+                //Enemy objesinden 10 tane oluştur
+                EnemyController newEnemy = Instantiate(_enemyPrefab);
+
+                //Oluşanları gizle ihtiyaç olunca çağırcaz
+                newEnemy.gameObject.SetActive(false);
+
+                //Sahnede birikmesin diye EnemyManager altında topla
+                newEnemy.transform.parent = this.transform;
+
+                //bunları kuyruğumuza(listemize) ekliyoruz)
+                _enemies.Enqueue(newEnemy);
+
+            }
+           
+        }
+        //Dışarıdan gelen düşmanı Pool havuzuna eklicek
+        public void SetPool(EnemyController enemyController)
+        {
+            //Düzen. Manager altına koy
+            enemyController.transform.parent = this.transform;
+
+            //Geri gönderileni tekrar gizle
+            enemyController.gameObject.SetActive(false);
+            //Parametre olarak geleni kuyruğa ekle
+            _enemies.Enqueue(enemyController);
+        }
+
+        //Enemy yi havuzdan çıkar yani kullan.
+        public EnemyController GetPool()
+        {
+            //Böyle bir nesne yoksa
+            if(_enemies.Count == 0)
+            {
+                //Bu fonksiyon ile oluştur
+                InitializePool();
+            }
+
+            //Böyle bir nesne varsa Sıradakini dön demek
+            return _enemies.Dequeue();
+        }
+    }//class
+}
+
